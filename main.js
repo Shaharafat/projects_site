f603532eb6a8050142b04e0229d70b0c69f63aa1
shaharafat <shaharafhat@gmail.com>
2020-08-07 01:54:47 +0600

    simple bug fixed

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 let project = document.querySelectorAll(".project");
 let project_count = document.querySelector(".project_count");
+let project_text = document.querySelector('.project-text');
 let i = 1;
+
 // counter
 setTimeout(function count() {
   if (i <= project.length) {
     let count = String(i).padStart(3, "0");
     project_count.innerHTML = count;
+    if (i == 2) {
+      project_text.innerHTML += 's'
+    }
     i++;
   } else {
     return;
@@ -13,3 +18,4 @@ setTimeout(function count() {
 
   setTimeout(count, 100);
 }, 100);
+
